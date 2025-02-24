3eaba63060bd541c15128d0bb55d438bca82a556
akadot <muriloholiveira@hotmail.com>
2025-02-24 19:30:49 -0300

    🚧 Starting login page

---
+++ web/pages/Auth.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const CAROUSSEL_IMGS = [
+    new URL('@/assets/imgs/caroussel_01.jpg', import.meta.url).href,
+    new URL('@/assets/imgs/caroussel_02.jpg', import.meta.url).href,
+];
+</script>
+
+<template>
+    <section class="d-flex flex-row min-h-screen">
+            <v-carousel cycle hide-delimiter-background show-arrows="hover">
+                <v-carousel-item v-for="(img, idx) in CAROUSSEL_IMGS" :key="'img' + idx" :absolute="false" height="100vh" max-width="50%">
+                    <v-img 
+                        :gradient="`to top, rgba(255,32,59,0.6), rgba(0,0,0,0.3)`" 
+                        :src="img"
+                        cover>
+                    </v-img>
+                </v-carousel-item>
+            </v-carousel>
+
+        <!-- Seção lateral com 25% da largura -->
+        <section>
+            <p>Bem-vindo ao sistema</p>
+            <v-img height="48" src="../assets/iconBlack.png"></v-img>
+        </section>
+    </section>
+</template>
--- web/plugins/vuetify.ts
+++ web/plugins/vuetify.ts
@@ -1,11 +1,30 @@
-import '@mdi/font/css/materialdesignicons.css'
+import '@mdi/font/css/materialdesignicons.css';
+import 'vuetify/styles';
+import { createVuetify, type ThemeDefinition} from 'vuetify';
 
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
+const MainTheme : ThemeDefinition = {
+  dark:false,
+  colors: {
+    primary:"#FF203B",
+    background: "#f4f4f4",
+    surface: "#f4f4f4",
+    mainBlack: "#0C0C0C",
+    secondary: "#484848",
+  }
+};
+
+const LightTheme : ThemeDefinition = {
+  dark:false,
+};
 
 export default defineNuxtPlugin((app) => {
   const vuetify = createVuetify({
-    // ... your configuration
+    theme: {
+      defaultTheme: 'MainTheme',
+      themes:{
+        MainTheme
+      }
+    }
   })
   app.vueApp.use(vuetify)
 })
